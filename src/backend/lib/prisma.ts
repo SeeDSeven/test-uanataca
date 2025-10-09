d8e62617b799e6d716a5d36fb1f6fe26d0869391
gogusr <comercial@seedseven.co>
2025-10-09 03:12:34 -0500

    resolviong dependencies

--- src/backend/lib/prisma.ts
+++ src/backend/lib/prisma.ts
@@ -1,14 +1,10 @@
 // backend/lib/prisma.ts
-
 import { PrismaClient } from '@prisma/client';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-// Simplemente creamos una nueva instancia de PrismaClient.
-// Prisma detectará automáticamente las variables de entorno de Vercel
-// y gestionará el pool de conexiones por sí mismo.
 export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
